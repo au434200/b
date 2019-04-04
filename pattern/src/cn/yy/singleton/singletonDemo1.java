fff7d0782d20e30d4b3356328ff5ec5f5a052f33
YY <619273103@qq.COM>
2019-04-04 12:26:47 +0800

    1

---
+++ pattern/src/cn/yy/singleton/singletonDemo1.java
@@ -0,0 +1,19 @@
+package cn.yy.singleton;
+
+/**
+ * 饿汉式1
+ * @author enter
+ *
+ */
+public class singletonDemo1 {
+	private singletonDemo1 s=new singletonDemo1();
+	
+	private singletonDemo1() {
+		
+	}
+	
+	public singletonDemo1 getSingletonDemo1() {
+		return s;
+	}
+
+}
